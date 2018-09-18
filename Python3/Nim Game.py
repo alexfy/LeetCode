257ad58c15ecafec52a4b865eab7ebc35934d727
alexfy <39016326+alexfy@users.noreply.github.com>
2018-09-18 09:07:53 -0400

    Add files via upload

---
+++ Python3/Nim Game.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 18 09:03:54 2018
+
+@author: hwny-in-fuy
+"""
+
+class Solution(object):
+    def canWinNim(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n%4!=0
