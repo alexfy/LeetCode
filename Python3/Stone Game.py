f36ec7baaec5c9481d3bac7437bad714f3755a60
alexfy <39016326+alexfy@users.noreply.github.com>
2018-10-09 14:48:02 -0400

    Add files via upload

---
+++ Python3/Stone Game.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  9 14:46:55 2018
+
+@author: hwny-fuy
+"""
+
+class Solution:
+    def stoneGame(self, piles):
+        """
+        :type piles: List[int]
+        :rtype: bool
+        """
+        return True
